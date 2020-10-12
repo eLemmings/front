e4ac8ead673436828ebf6daa30f3c8f268b661fc
kamiloox <mrkamiloox@gmail.com>
2020-10-12 22:09:11 +0200

    Stworzenie wstępnego widoku logowania

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import './index.module.scss';
+import styles from './view.module.scss';
+import Button from '@material-ui/core/Button';
 
-function App() {
-  return (
-    <div className="App">
-      
+const App = () => (
+  <div className={styles.wrapper}>
+    <div className={styles.logo}>
+      <div className={styles.icon}></div>
+      <h1 className={styles.projectName}>Lem<span className={styles.greenFont}>Med</span></h1>
     </div>
-  );
-}
+    <div className={styles.wrapperLogin}>
+      <p className={styles.marginBottom7}>Witaj w Lem<span className={styles.greenFont}>Med</span></p>
+      <p className={styles.font13}>Zaloguj się aby kontynuować</p>
+    </div>
+  </div>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import App from './App';
 import ReactDOM from 'react-dom';
+import React from 'react';
 
-ReactDOM.render(App, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
