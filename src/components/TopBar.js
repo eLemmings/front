b023bbb7ece605e258bcf35ac336d755de222a56
kamiloox <mrkamiloox@gmail.com>
2020-10-20 21:13:52 +0200

    Dodano plusik

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,19 +1,19 @@
 import React from "react";
 import Button from "./Button";
 import styles from "./scss/TopBar.module.scss";
-import Menu from "./Menu";
+// import Menu from "./Menu";
 
 const TopBar = ({ handleChangeFn, title, options, activeItem }) => {
-	return (
-		<div className={styles.wrapper}>
-			{/* <Menu options={options} activeItem={activeItem} /> */}
-			<div className={styles.center}>
-				<Button handleFn={() => handleChangeFn(-1)}>&lt;</Button>
-				<p>{title}</p>
-				<Button handleFn={() => handleChangeFn(1)}>&gt;</Button>
-			</div>
-		</div>
-	);
+  return (
+    <div className={styles.wrapper}>
+      {/* <Menu options={options} activeItem={activeItem} /> */}
+      <div className={styles.center}>
+        <Button handleFn={() => handleChangeFn(-1)}>&lt;</Button>
+        <p>{title}</p>
+        <Button handleFn={() => handleChangeFn(1)}>&gt;</Button>
+      </div>
+    </div>
+  );
 };
 
 export default TopBar;
